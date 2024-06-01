3d71ed6c8e194b92d6714106e5a6312ee76d8e35
Danil <daniltonkikhpr@mail.ru>
2024-06-01 13:45:53 +0300

    commit

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,8 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+    //dcwcw
+
     void Start()
     {
         Time.timeScale = _timeScale;
